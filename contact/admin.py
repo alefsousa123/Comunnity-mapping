e7aea557f1885604dd6030b5f95482032de1e1a1
alefsousa123 <lef.azevedo123@gmail.com>
2025-06-18 15:29:30 -0300

    feat: add category and owner fields to Contact model, update admin interface and migrations

--- contact/admin.py
+++ contact/admin.py
@@ -12,18 +12,28 @@ class ContactAdmin(admin.ModelAdmin):
         "phone",
         "email",
         "created_date",
+        "category",
+    
     )
     search_fields = ("first_name", "last_name", "phone", "email")
     list_per_page = 10
     list_max_show_all = 200
-    list_editable = (
-        "first_name",
-        "last_name",
-    )
+    # list_editable = (
+    #     "first_name",
+    #     "last_name",
+    # )
     list_display_links = (
         "id",
         "phone",
         "email",
+        "category",
     )
     # list_filter = ("created_date",)
+    ordering = ("-id",)
+
+
+@admin.register(models.Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+
     ordering = ("-id",)
--- contact/models.py
+++ contact/models.py
@@ -1,16 +1,29 @@
+from os import name
 from venv import create
 from django.db import models
 from django.utils import timezone
-
+from django.contrib.auth.models import User
 # Create your models here.
 
 # id (primary key - automático)
 # first_name (string), last_name (string), phone (string)
 # email (email), created_date (date), description (text)
 
+
 # Depois
 # category (foreign key), show (boolean), owner (foreign key)
 # picture (imagem)
+class Category(models.Model):
+    class Meta:
+        verbose_name = "Category"
+        verbose_name_plural = "Categories"
+
+    
+    name = models.CharField(max_length=50, unique=True)
+
+    def __str__(self):
+        """String representation of the Category model."""
+        return self.name
 
 
 class Contact(models.Model):
@@ -22,6 +35,12 @@ class Contact(models.Model):
     created_date = models.DateTimeField(default=timezone.now)
     show = models.BooleanField(default=True)
     picture = models.ImageField(blank=True, upload_to="pictures/%Y/%m/")
+    category = models.ForeignKey(
+        Category, on_delete=models.SET_NULL, blank=True, null=True
+    )
+    owner = models.ForeignKey(
+        User, on_delete=models.SET_NULL, blank=True, null=True
+    )
 
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
